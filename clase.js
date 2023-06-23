e207a894e202bb94f6dd95d395ea264b8b27ba02
Jacobo Tomas Lerner Rojas <j.lernerrojas@gmail.com>
2023-06-23 14:44:27 -0300

    cambios de segundo desafio

--- clase.js
+++ clase.js
@@ -1,43 +1,82 @@
+import fs from "fs/promises"
+const path ="./productos.json"
+
 
 class ProductManager {
-    #id = 0;
+    #id = 1;
     constructor() {        
-        this.products = [];
+        this.products=[];
     }
 
-    getProducts = () => {
-        return this.products;
+    getProducts = async () => {
+        const file = await fs.readFile(path, "utf-8");
+        const products = JSON.parse(file)
+        return products;
     }
 
-    addProduct(product) { 
-        const {title,description,price,thumbnail,code,stock}= product;
-
-        if (!title || !description || !price || !thumbnail || !code || !stock) {
-            console.log("Datos incompletos");
-            return null; 
+    addProduct= async (product)=> { 
+        try{
+            const file=await fs.readFile(path,"utf-8");
+            const products=JSON.parse(file);
+            const {title,description,price,thumbnail,code,stock}= product;
+            this.products=products
+            if (products.length === 0) {
+                this.#id = 1
+            } else {
+                this.#id = products[products.length-1].id + 1
+            }
+            if (!title || !description || !price || !thumbnail || !code || !stock) {
+                console.log("Datos incompletos");
+                return null; 
+            }   
+            const newProduct ={
+                id:this.#id++,
+                ...product,
+            };
+            products.push(newProduct);
+            await fs.writeFile(path, JSON.stringify(products));
+            return newProduct        
         }
+        catch{(e)=>{
+            console.log(e)
+        } }
 
-        // Verificar que el código no esté en uso
-        const productWithCode = this.products.find((product) => product.code === code);
-        if (productWithCode) {
-            throw new Error("El código del producto ya está en uso. Revisa que no hayan productos con el mismo código e intenta nuevamente.");
         }
-        const newProduct ={
-            id:this.#id++,
-            ...product,
-        };
-        this.products.push(newProduct);
-    }
-
-    getProductById(productId){
-        const product = this.products.find(product => product.id === productId);
-        if(!product||undefined){
-            throw new Error( "NOT FOUND. Por favor, ingrese un id válido.")
-        } else if(product){
-            console.log ("El producto "+ product.id +"," + product.title + " fue encontrado con exito");
-            return product;
-        } 
-    }
+        getProductById = async (productId)=>{
+                const file=await fs.readFile(path,"utf-8");
+                const products=JSON.parse(file);
+                const product = products.find(product => product.id === productId);
+                if(product){
+                    console.log(product)
+                    return product;
+                } else{
+                    console.log("El producto no existe")
+                    return "El producto no existe"
+                }}
+        
+        updateProduct = async (id,obj) => {
+            const file = await fs.readFile(path,"utf-8");
+            const products=JSON.parse(file);
+            const indexProduct = products.findIndex(p=>p.id === id)
+            if(indexProduct === -1){
+                return 'Producto NO encontrado'
+            }
+            const productUpdate = {...products[indexProduct],...obj}
+            products.splice(indexProduct,1,productUpdate)
+            await fs.writeFile(path,JSON.stringify(products))
+        }
+        
+        deleteProductbyId=async(deleteId)=>{
+            try{
+                const file=await fs.readFile(path,"utf-8");
+                const products=JSON.parse(file);
+                const nuevoProductos=products.filter(product=>product.id!= deleteId)
+                await fs.writeFile(path, JSON.stringify(nuevoProductos));
+                nuevoProductos=products
+                return products
+            }
+            catch{(e)=>{console.log(e)}}
+        }
 
 }
 
@@ -57,7 +96,7 @@ const playstation = {
     description: 'La revolucion esta aca',
     price: 500000,
     thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxszX6ODgsUYH_hsUmMwdwI1Q271N7hXlB41W49q6Jx2dwqkSZ9176QsUKipNWTsJkJ0U&usqp=CAU',
-    code: '1UNGMsz9opopRIkGPt0y', //Pon el mismo codigo que en el producto de arriba para ver el error en consola 6CanoVg64jPxh3EWvhMm
+    code: '1UNGMsz9opopRIkGPt0y', //Pon el mismo codigo que en el producto de arriba para ver el error en consola 6CanoVg64jPxh3EWvhMm 1UNGMsz9opopRIkGPt0y
     stock: 5 
 }
 
@@ -70,10 +109,13 @@ const televisor = {
     stock: 10
 }
 
-productManager.addProduct(playstation);
-productManager.addProduct(leche);
-productManager.addProduct(televisor);
+await productManager.addProduct(playstation);
+await productManager.addProduct(leche);
+await productManager.addProduct(televisor);
 
+console.log(await productManager.getProducts())
 
-
-productManager.getProductById(2); // Busque con un numero mayor de los productos registrados para dar el simulacro de error
+await productManager.getProductById(2); // Busque con un numero mayor de los productos registrados para dar el simulacro de error
+await productManager.deleteProductbyId(1);
+await productManager.updateProduct(4,{marca: "Argentina"})
+console.log(await productManager.getProducts())
