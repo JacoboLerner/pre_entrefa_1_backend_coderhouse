dea28079f3148db94fa92b3f2998e5f699c1e39a
Jacobo Tomas Lerner Rojas <j.lernerrojas@gmail.com>
2023-07-12 12:50:03 -0300

    cambios hacia cuarto desafio

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import express from "express";
 import productsRouter from "./routes/productsRoutes.js";
 import cartRouter from "./routes/cartRoutes.js";
+import { fileURLToPath } from 'url';
+import path from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express()
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-
+app.use("/assets",express.static( __dirname + "/public"));
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartRouter)
 
